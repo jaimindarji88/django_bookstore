f44783d6b06f7715d925d103f0ffb9df9d484aa0
Jaimin Darji <jaimin.d.88@gmail.com>
2016-09-15 20:00:46 -0400

    fixed spelling

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -131,3 +131,14 @@ STATIC_URL = '/static/'
 
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/store/'
+
+#Email Backends
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "jasminde77@gmail.com"
+EMAIL_HOST_PASSWORD = "jasminder88"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "DANKMEMES.com"
