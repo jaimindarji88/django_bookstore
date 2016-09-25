e161a1b002fd25c23f3a84d89f6f018cc1dd01ba
Jaimin Darji <jaimin.d.88@gmail.com>
2016-09-25 18:53:33 -0400

    added cart/add_to_cart/remove_from_cart and cart template

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from views import store, book_details
+from views import store, book_details, add_to_cart, remove_from_cart, cart
 
 urlpatterns = [
     url(r'^$', store, name='index'),
-    url(r'^book/(\d+)', book_details, name='book_details')
+    url(r'^book/(\d+)', book_details, name='book_details'),
+    url(r'^add/(\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^remove/(\d+)', remove_from_cart, name='remove_from_cart'),
+    url(r'^cart/', cart, name='cart'),
 ]
